812a727f164ea52dc62c68289e57761260627cc9
Ryan Karpen <ryankarpen2015@gmail.com>
2024-06-11 22:44:06 -0300

    alteração BASE_URL

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import Questionario from "@/components/Questionario";
 import { useRouter } from "next/router";
 
 
-const BASE_URL = "http://localhost:3000/api"
+const BASE_URL = "https://quiznext.vercel.app/api"
 
 
 export default function Home() {
